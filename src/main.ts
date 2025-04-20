ef43739561d795e908875a2907f3ca8623abb9dd
Moonsoo <aptx15hibr@naver.com>
2025-04-21 08:09:21 +0900

    template 구문 추가

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,45 @@
-import './assets/main.css'
+// 디폴트 main.ts
+// import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+// import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+// import App from './App.vue'
+// import router from './router'
 
+// const app = createApp(App)
+
+// app.use(createPinia())
+// app.use(router)
+
+// app.mount('#app')
+
+// render() 함수로 Vue 앱을 만들기
+
+// const App = {
+//   render() {
+//     return "This is the app's entrance"
+//   },
+// }
+// const app = createApp(App)
+// app.mount('#app')
+// tmplate 방식으로 Vue 앱을 만들기
+
+// const Description = {
+//   template: "This is the app's entrance",
+// }
+
+// const App = {
+//   components: { Description },
+//   template: '<Description />',
+// }
+
+// const app = createApp(App)
+// app.mount('#app')
+
+const App = {
+  template: `<h1>This is the app's entrance</h1>
+    <h2>We are exploring template syntax</h2>`,
+}
 const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
 app.mount('#app')
