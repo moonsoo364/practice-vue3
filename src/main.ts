e31d738ed72a9246142729ecf271aad624195c98
moonsoo.kwon <moonsoo.kwon@seeroo.co.kr>
2025-04-21 09:05:05 +0900

    컴포넌트 Data 실습습

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,20 @@
-// 디폴트 main.ts
-// import './assets/main.css'
+//2-5 Component Data 
+import { createApp, type ComponentOptions } from 'vue'
 
-import { createApp } from 'vue'
-// import { createPinia } from 'pinia'
-
-// import App from './App.vue'
-// import router from './router'
-
-// const app = createApp(App)
-
-// app.use(createPinia())
-// app.use(router)
-
-// app.mount('#app')
-
-// render() 함수로 Vue 앱을 만들기
-
-// const App = {
-//   render() {
-//     return "This is the app's entrance"
-//   },
-// }
-// const app = createApp(App)
-// app.mount('#app')
-// tmplate 방식으로 Vue 앱을 만들기
-
-// const Description = {
-//   template: "This is the app's entrance",
-// }
-
-// const App = {
-//   components: { Description },
-//   template: '<Description />',
-// }
-
-// const app = createApp(App)
-// app.mount('#app')
+type Data ={title : string;}
 
 const App = {
-  template: `<h1>This is the app's entrance</h1>
-    <h2>We are exploring template syntax</h2>`,
+  template : `<h1>{{title}}</h1>`,
+    data() : Data {
+      return{
+      title : 'My First Vue component',
+    }
+  },
+  created(){
+    console.log((this as ComponentOptions<Data>).title)
+  }
 }
+
 const app = createApp(App)
 app.mount('#app')
+
