38f8dfb8eba84219dadccdec08dd6487cb3fde8d
moonsoo.kwon <moonsoo.kwon@seeroo.co.kr>
2025-04-24 12:40:58 +0900

    3장 시작

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
 import { createApp } from 'vue'
 
-const app = createApp({template: '<MyComponent />'});
-const MyComponent = {
-  template: 'This is my global component' // 'templage' -> 'template'로 수정
-}
+// const app = createApp({template: '<MyComponent />'});
+// const MyComponent = {
+//   template: 'This is my global component' // 'templage' -> 'template'로 수정
+// }
 
-app.component('MyComponent', MyComponent)
-app.mount('#app')
+// app.component('MyComponent', MyComponent)
+// app.mount('#app')
+
 //SFC파일
+import MyComponent from './components/do/MyComponent.vue';
+
+// import './assets/main.css'
+//원본 코드
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.component('MyComponent', MyComponent);
+app.use(router)
+app.mount("#app")
