dcb1385e68fa2f297c67c0c3735c207dd259c040
moonsoo.kwon <moonsoo.kwon@seeroo.co.kr>
2025-04-28 09:07:25 +0900

    훅 로그 작성

---
+++ src/components/do/MyFirsrComponent.vue
@@ -0,0 +1,32 @@
+<template>
+ <h2 class="heading">I am {{ message }}</h2>
+ <input v-model="message" type="text" placeholder="Enter your name"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'MyFirstComponent',
+  data() {
+    return {
+      message: '',
+    }
+  },
+  setup() {
+    console.log('setup hook triggered!');
+    return {};
+  },
+  beforeCreate() {
+    console.log("beforeCreate hook triggered!");
+  },
+  created(){
+    console.log('created hook triggered');
+  },
+  beforeMount(){
+    console.log('beforeMount hook triggered');
+  },
+  mounted() {
+    console.log('mounted hook triggered');
+  }
+})
+</script>
