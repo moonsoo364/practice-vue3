8a0670ed5e2398088007511612ba6543501cca33
moonsoo.kwon <moonsoo.kwon@seeroo.co.kr>
2025-04-30 09:16:07 +0900

    ref 실습 진행

---
+++ src/components/do/ch03/3_9_ref_exam.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <input type="text" ref="messageRef" placeholder="Enter a message"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MyComponent',
+})
+</script>
---
+++ src/ts_class/Task.ts
@@ -0,0 +1,6 @@
+export interface Task {
+    id: string;
+    description: string;
+}
+
+// Removed redundant export to avoid conflict
