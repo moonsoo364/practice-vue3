5fe99cffcd9edf6bde3469a4f4793ac75576e553
moonsoo.kwon <moonsoo.kwon@seeroo.co.kr>
2025-05-09 09:03:21 +0900

    axios 실습

---
+++ src/components/do/ch06/6_2_axios_get/UserProfile.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import axios from 'axios'
+import {ref} from 'vue'
+
+const user = ref(null)
+const err = ref(null)
+
+axios.get('https://api.github.ocm/users/mayashavin')
+.then(response=>{
+    user.value = response.data;
+})
+//async, await
+async function getUser(){
+    const response = await axios.get('https://api.github.ocm/users/mayashavin');
+    user.value = response.data;
+}
+getUser();
+//try catch
+async function getUser1(){
+    try{
+        const response = await axios.get('https://api.github.ocm/users/mayashavin');
+        user.value = response.data;
+    }catch(error){
+        err.value = error;
+    }
+
+}
+getUser();
+</script>
