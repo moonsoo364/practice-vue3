280ce391db121fedbb7a45148102b86211d81e81
Moonsoo <aptx15hibr@naver.com>
2025-05-23 08:10:56 +0900

    feat) 카트 삭제 메서드 추가

--- src/components/do/ch09/CartComponent.vue
+++ src/components/do/ch09/CartComponent.vue
@@ -1,47 +1,51 @@
 <template>
-    <div class="cart">
-        <span class="cart__total" @click="showCartDetails = !showCartDetails" >Cart : {{ cart.total }}</span>
-        <ul class="cart__list" v-show="showCartDetails">
-            <li class="cart__list-item" v-for="item in cart.items" :key="item.id">
-                <span>Id : {{ item.id }}</span>
-                <span>Quantity: {{ item.quantity }}</span>
-            </li>
-        </ul>
-    </div>
+  <div class="cart">
+    <span class="cart__total" @click="showCartDetails = !showCartDetails"
+      >Cart : {{ cart.total }}</span
+    >
+    <ul class="cart__list" v-show="showCartDetails">
+      <li class="cart__list-item" v-for="(item, index) in cart.detailedItems" :key="item.id">
+        <span>{{ index + 1 }}. {{ item.title }}</span>
+        <span>${{ item.price }}</span> x
+        <span>{{ item.quantity }}</span>
+        <span>{{ item.total }}</span>
+        <button @click="cart.remove(item.id)">Remove</button>
+      </li>
+    </ul>
+    <button @click="cart.clear">Remove all</button>
+  </div>
 </template>
 <script setup lang="ts">
-    import { useCartStore } from '@/stores/ch09/cart';
-    import {ref} from 'vue';
-    const cart = useCartStore();
-    const showCartDetails = ref(false);
-
-
+import { useCartStore } from '@/stores/ch09/cart'
+import { ref } from 'vue'
+const cart = useCartStore()
+const showCartDetails = ref(false)
 </script>
 <style scoped>
 .cart__list {
-    position: absolute;
-    list-style: none;
-    box-shadow: 2px 2px 3px #e3e0e0;
-    border: 1px solid #e3e0e0;
-    padding: 10px;
-    inset-inline-end: 0;
-    background-color: white;
-    min-width: 500px
+  position: absolute;
+  list-style: none;
+  box-shadow: 2px 2px 3px #e3e0e0;
+  border: 1px solid #e3e0e0;
+  padding: 10px;
+  inset-inline-end: 0;
+  background-color: white;
+  min-width: 500px;
 }
 
 .cart {
-    position: relative;
+  position: relative;
 }
 
 .cart__total {
-    cursor: pointer;
-    text-decoration: underline;
+  cursor: pointer;
+  text-decoration: underline;
 }
 
 .cart__list-item {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 5px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 5px;
 }
 </style>
--- src/stores/ch09/cart.ts
+++ src/stores/ch09/cart.ts
@@ -1,36 +1,67 @@
-import {defineStore} from 'pinia'
-import {reactive, computed} from 'vue'
+import { defineStore } from 'pinia'
+import { reactive, computed } from 'vue'
+import { usePizzasStore } from '../pizzas'
 
 type CartItem = {
-    id: string;
-    quantity: number;
+  id: string
+  quantity: number
 }
 
-export const useCartStore = defineStore('cart',() =>{
-    const items = reactive<CartItem[]>([]);
+export const useCartStore = defineStore('cart', () => {
+  const items = reactive<CartItem[]>([])
 
-    const total =computed(()=>{
-        return items.reduce((acc, item) => {
-            return acc + item.quantity;
-        }, 0)
-    })
-    
-    const add = (item: CartItem) => {
-        const index =items.findIndex(i => i.id === item.id)
-        if(index > -1){
-            items[index].quantity += item.quantity;
-        }else{
-            items.push(item)
-        }
-        return{
-            items,
-            total,
-            add
-        }
+  const total = computed(() => {
+    return items.reduce((acc, item) => {
+      return acc + item.quantity
+    }, 0)
+  })
+
+  const add = (item: CartItem) => {
+    const index = items.findIndex((i) => i.id === item.id)
+    if (index > -1) {
+      items[index].quantity += item.quantity
+    } else {
+      items.push(item)
     }
     return {
-        items,
-        total,
-        add
+      items,
+      total,
+      add,
     }
+  }
+  const detailedItems = computed(() => {
+    const pizzasStore = usePizzasStore()
+
+    return items.map((item) => {
+      const pizza = pizzasStore.pizzas.find((pizza) => pizza.id === item.id)
+      const pizzaPrice = pizza?.price ? +pizza?.price : 0
+
+      return {
+        ...item,
+        title: pizza?.title,
+        price: pizza?.price,
+        total: pizzaPrice * item.quantity,
+      }
+    })
+  })
+
+  const remove = (id: string) => {
+    const index = items.findIndex((item) => item.id === id)
+    if (index > -1) {
+      items.splice(index, 1)
+    }
+  }
+
+  const clear = () => {
+    items.length = 0
+  }
+
+  return {
+    items,
+    total,
+    add,
+    detailedItems,
+    remove,
+    clear,
+  }
 })
